44eb16c359252649ae2933bf359b5a21487cee16
Edem Ziddah <contact@codevoweb.com>
2022-11-09 13:05:57 +0000

    updated

--- app/note_serializers.py
+++ app/note_serializers.py
@@ -10,17 +10,5 @@ def noteEntity(note) -> dict:
     }
 
 
-def populatednoteEntity(note) -> dict:
-    return {
-        "id": str(note["_id"]),
-        "title": note["title"],
-        "category": note["category"],
-        "content": note["content"],
-        "published": note["published"],
-        "createdAt": note["createdAt"],
-        "updatedAt": note["updatedAt"]
-    }
-
-
 def noteListEntity(notes) -> list:
-    return [populatednoteEntity(note) for note in notes]
+    return [noteEntity(note) for note in notes]
